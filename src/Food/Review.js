ae738be8369168fa7719d6e5df01c7d96bed60a5
= <=>
2022-09-18 00:54:19 -0500

    Added API gateways for reviews

--- src/Food/Review.js
+++ src/Food/Review.js
@@ -1,35 +1,49 @@
-import { useEffect } from 'react'
+import { useState } from 'react'
 import styles from './Review.module.scss'
 import FlipCard from '../components/FlipCard.js'
 import StarMeter from '../components/StarMeter.js'
+import Modal from '../components/Modal.js'
+import Article from './Article.js'
 
 function Review(props) {
+    const [visible, setVisible] = useState(false);
+    const toggleArticle = () => {
+        setVisible(!visible)
+    }
     return (
-        <div className={styles.review}>
-            <FlipCard>
-                <div className={[styles.card, styles.front].join(" ")}>
-                    <img width="400px" height="350px" src={props.image}></img>
-                    <div className={styles.heading}>    
-                        <h1>{props.title}</h1>
+        <>
+            <div className={styles.review}>
+                <FlipCard>
+                    <div className={[styles.card, styles.front].join(" ")}>
+                        <img width="400px" height="350px" src={props.image}></img>
+                        <div className={styles.heading}>    
+                            <h1>{props.title}</h1>
+                        </div>
                     </div>
-                </div>
-                <div className={[styles.card, styles.back].join(" ")}>
-                    <StarMeter value={props.rating}/>
-                    <p>
-                        <i>
-                            "{props.summary}"
-                        </i>
-                    </p>
-                    <div className={styles.wrapper}>
-                        <a href="/">
-                            <button>
+                    <div className={[styles.card, styles.back].join(" ")}>
+                        <StarMeter value={props.rating}/>
+                        <p>
+                            <i>
+                                "{props.summary}"
+                            </i>
+                        </p>
+                        <div className={styles.wrapper}>
+                            <button onClick={toggleArticle}>
                                 READ MORE
                             </button>
-                        </a>
+                        </div>
                     </div>
-                </div>
-            </FlipCard>
-        </div>
+                </FlipCard>
+            </div>
+            {visible && 
+                <Modal control={toggleArticle}>
+                    <Article
+                        title={props.title}
+                        body={props.body}
+                    />
+                </Modal>
+            }
+        </>
     );
 }
 
--- src/Food/Reviews.js
+++ src/Food/Reviews.js
@@ -1,18 +1,57 @@
-import { useEffect } from 'react'
 import Review from './Review';
 import styles from './Reviews.module.scss'
-import data from  '../json/Reviews.json'
+import axios, { CancelToken } from "axios";
+import React, { useState, useEffect} from "react";
 
 function Reviews() {
+    const [reviews, setReviews] = useState([]);
+    const [refreshKey, setRefreshKey] = useState(0);
+
+    useEffect(() => {
+        const cancelTokenSource = CancelToken.source();
+    
+        async function loadReviews() {
+          try {
+            var config = {
+                method: 'post',
+                url: 'https://395d7mrcx6.execute-api.us-east-2.amazonaws.com/prod/reviews/get-all-reviews',
+                headers: {"Content-Type":"application/json"},
+                data : '',
+                cancelToken: cancelTokenSource.token,
+            };
+            
+            const asyncResponse = await axios(config);
+            const { reviews } = asyncResponse.data;
+            
+            setReviews(reviews);
+          } catch (err) {
+            if (axios.isCancel(err)) {
+              return console.info(err);
+            }
+            console.error(err);
+          }
+        }
+    
+        loadReviews();
+    
+        return () => {
+          // here we cancel preveous http request that did not complete yet
+          cancelTokenSource.cancel(
+            "Cancelling previous http call because a new one was made ;-)"
+          );
+        };
+    }, [refreshKey]);
+
     return (
         <div className={styles.reviews}>     
             <div className={styles.flexContainer}>
-                {data.map((el, idx) => (
+                {reviews.map((el, idx) => (
                     <Review key={idx}
                         image={el.image}
-                        title={el.title}
+                        title={decodeURIComponent(el.title)}
                         rating={el.rating}
-                        summary={el.summary}
+                        summary={decodeURIComponent(el.summary)}
+                        body={decodeURIComponent(el.body)}
                     />
                 ))}
             </div>
