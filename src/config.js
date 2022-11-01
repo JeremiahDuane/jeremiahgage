43bb3ffd2838838f0bededd472e2bf712aee1900
= <=>
2022-11-01 01:18:34 -0500

    Remove obsolete key

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     github: {
         site_url: "https://github.com/JeremiahDuane/jeremiahgage",
         api_url: "https://api.github.com",
-        api_auth: "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"
+        api_auth: ""
     } 
 }
 export default config
