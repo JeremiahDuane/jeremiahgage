e9de73b8be1cfbb4206054c860cf00ad4742bf3d
= <=>
2023-03-31 00:07:51 -0500

    License added to footer.

--- src/Components/Software/Footer/Footer.js
+++ src/Components/Software/Footer/Footer.js
@@ -2,6 +2,6 @@ import styles from './Footer.module.scss'
 
 export default function Landing() {
     return (
-        <div style={{position: "fixed", right: "20px", bottom: "20px"}}>v1.0.7</div>
+        <div className={styles.footer}> GPL-3.0 license | v1.0.7</div>
     );
 }
